76efc761bf117d82cd55ac840c858169d6779f03
Jimbo-Qu <qujiangbo@shinetechchina.com>
2016-06-08 09:35:15 +0800

    add user controller unit test

--- Shinetech.PlanPoker/Shinetech.PlanPoker.WebApi.Tests/UserControllerTest.cs
+++ Shinetech.PlanPoker/Shinetech.PlanPoker.WebApi.Tests/UserControllerTest.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Moq;
+using Shinetech.PlanPoker.WebApi.ViewModels;
+using Shinetech.PlanPoker.ILogic;
 
 namespace Shinetech.PlanPoker.WebApi.Tests
 {
    [TestFixture]
     public class UserControllerTest
     {
+        private Mock<IUserLogic> _iuserLogicMock;
+
         [SetUp]
         public void SetUp() {
-
+            _iuserLogicMock = new Mock<IUserLogic>();
         }
 
         [TearDown]
@@ -23,11 +23,43 @@ namespace Shinetech.PlanPoker.WebApi.Tests
         [Test]
         public void Check_email_when_email_exists_should_return_true() {
             //Arrange
+            var userViewModelMock = new UserViewModel
+            {
+                Email="123456@qq.com",
+                Password="123456"
+            };
 
+            
             //Act
 
             //Assert
         }
-        
+
+        [Test]
+        public void Check_email_when_email_does_not_exist_should_return_false() {
+            //Arrange
+            var userViewModelMock = new UserViewModel
+            {
+                Email = "123456@qq.com",
+                Password = "123456"
+            };
+            //Act
+
+            //Assert
+        }
+
+        [Test]
+        public void Check_email_when_email_does_not_exist_should_return_false_and_create_user()
+        {
+            //Arrange
+            var userViewModelMock = new UserViewModel
+            {
+                Email = "123456@qq.com",
+                Password = "123456"
+            };
+            //Act
+
+            //Assert
+        }
     }
 }
