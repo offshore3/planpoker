f3c1ab337a59dce4cfbb26ade5cbabe94e840c3a
Jimbo-Qu <qujiangbo@shinetechchina.com>
2016-06-08 14:29:29 +0800

    add profile page controller, service and business logic

---
+++ Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/controller/profileController.js
@@ -0,0 +1,34 @@
+﻿appModule.controller('profileController', ['$scope', 'profileService', function ($scope, profileService) {
+    $scope.user = {};
+    $scope.user.image = "/images/user.png";
+
+}]);
+
+
+
+
+            
+
+            //profileService.getUser().then(function (response) {
+            //    $scope.user = response.data;
+            //    if (response.data.image != null || response.data.Image !== '') {
+            //        $scope.user.image = response.data.Image;
+            //    }
+            //    else {
+            //        $scope.user.image = "/images/user.png";
+            //    }
+            //});
+
+            //$scope.updateUser = function () {
+            //    loginService.login($scope.user.username, $scope.user.oldpassword).then(function (response) {
+            //        console.log(response.data.Status);
+            //        if (response.data.Status) {
+            //            profileService.editUser($scope.user).then(function () {
+            //                $scope.message = "update sucess";
+            //                $scope.isStatus = true;
+            //            });
+            //        } else {
+            //            $scope.message = "old password is wrong";
+            //            $scope.isStatus = false;
+            //        }
+            //    });
---
+++ Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/service/profileService.js
@@ -0,0 +1,20 @@
+﻿appModule.service("profileService", ['$http', '$q', function ($q,$http) {
+    this.getUser = function (userId) {
+        return $http({
+            method: "Get",
+            url: webAPI + "/api/user/" + userId
+        }).then(function (response) {
+            return $q.when(response);
+        });
+    }
+
+    this.editUser = function (user) {
+        return $http({
+            method: "Put",
+            url: webAPI + "/api/user",
+            data: user
+        }).then(function (response) {
+            return $q.when(response);
+        });
+    }
+}]);
