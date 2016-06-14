e699c331a1d0a0589aec32b5dcbc36fd612df139
JoyZhang <zhangyi@shinetechchina.com>
2016-06-14 15:39:38 +0800

    Add cookies check if user is logout he cannot go to dashboard page.

--- Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/route/shinetechApp.js
+++ Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/route/shinetechApp.js
@@ -3,12 +3,16 @@ var Token = "Token";
 var userImagePath = "";
 var appModule = angular.module("shinetech-app", ["ngRoute", "ngCookies"]);
 
-angular.module("shinetech-app").run(["$rootScope", "$location", "$routeParams", function ($rootScope, $location, $routeParams) {
+angular.module("shinetech-app").run(["$rootScope", "$location", "$routeParams", '$cookieStore', function ($rootScope, $location, $routeParams, $cookieStore) {
     $rootScope.$on("$routeChangeSuccess", routeChangeSuccess);
     function routeChangeSuccess() {
         $rootScope.isSettingsShow = true;
         if ($location.path() === "/login" || $location.path() === "/register" || $location.path() === "/resetpassword") {
             $rootScope.isSettingsShow = false;
+        } else {
+            if ($cookieStore.get('LoginUserId') === undefined || $cookieStore.get('LoginUserId') === null) {
+                $location.path("/login");
+            }
         }
     }
 
