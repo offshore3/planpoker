91ee34da1b1e2553e1789b07eea9f73bad5e10f8
JoyZhang <zhangyi@shinetechchina.com>
2016-06-14 15:51:26 +0800

    fix a small bug when user try to reset his password he will redirect to login page.

--- Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/route/shinetechApp.js
+++ Shinetech.PlanPoker/Shinetech.PlanPoker/scripts/route/shinetechApp.js
@@ -3,17 +3,19 @@ var Token = "Token";
 var userImagePath = "";
 var appModule = angular.module("shinetech-app", ["ngRoute", "ngCookies"]);
 
-angular.module("shinetech-app").run(["$rootScope", "$location", "$routeParams", '$cookieStore', function ($rootScope, $location, $routeParams, $cookieStore) {
-    $rootScope.$on("$routeChangeSuccess", routeChangeSuccess);
-    function routeChangeSuccess() {
-        $rootScope.isSettingsShow = true;
-        if ($location.path() === "/login" || $location.path() === "/register" || $location.path() === "/resetpassword") {
-            $rootScope.isSettingsShow = false;
-        } else {
-            if ($cookieStore.get('LoginUserId') === undefined || $cookieStore.get('LoginUserId') === null) {
-                $location.path("/login");
+angular.module("shinetech-app").run([
+    "$rootScope", "$location", "$routeParams", "$cookieStore", function($rootScope, $location, $routeParams, $cookieStore) {
+        $rootScope.$on("$routeChangeSuccess", routeChangeSuccess);
+
+        function routeChangeSuccess() {
+            $rootScope.isSettingsShow = true;
+            if ($location.path() === "/login" || $location.path() === "/register" || $location.path() === "/retrievepassword") {
+                $rootScope.isSettingsShow = false;
+            } else {
+                if ($cookieStore.get("LoginUserId") === undefined || $cookieStore.get("LoginUserId") === null) {
+                    $location.path("/login");
+                }
             }
         }
     }
-
-}]);
+]);
